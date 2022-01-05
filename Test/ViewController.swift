8008a3793f9f593b07250544838b8ff067886963
Timur Nigmatov <timelandchoco@yandex.ru>
2022-01-05 12:13:20 +0500

    Экран авторизации

---
+++ Test/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Timur Nigmatov on 04.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
